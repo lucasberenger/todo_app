51360052081cdb926eff0229215a23bb615c3f03
lucasberenger <envtux@gmail.com>
2025-09-03 19:27:24 -0300

    create basic tests

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' // dev database
 SQLALCHEMY_DATABASE_URL = 'postgresql://admin:admin@localhost:5432/todo'
 
 
